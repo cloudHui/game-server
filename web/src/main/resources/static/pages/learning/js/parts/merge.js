68f33e13ac4f2a7554fd194107c4fac071777eca
liuyunhui <1056405256@qq.com>
2026-07-30 03:40:21 +0000

    问题：merge.js 把分包数据写到了 data 函数本身，Vue 调用后仍得到空对象，导致 regOptions 未定义。
      修改：
    
      - 改为调用各分包 data 后合并成完整对象再返回。
      - JS 版本号更新为 20260730-1，避免手机继续使用旧缓存

--- web/src/main/resources/static/pages/learning/js/parts/merge.js
+++ web/src/main/resources/static/pages/learning/js/parts/merge.js
@@ -2,11 +2,19 @@
 window.LearningParts = window.LearningParts || [];
 window.LearningMerge = function (base) {
   var parts = window.LearningParts || [];
+  var dataSources = [base.data];
   parts.forEach(function (part) {
-    if (part.data) Object.assign(base.data, typeof part.data === 'function' ? part.data() : part.data);
+    if (part.data) dataSources.push(part.data);
     if (part.computed) Object.assign(base.computed, part.computed);
     if (part.methods) Object.assign(base.methods, part.methods);
   });
+  base.data = function () {
+    var merged = {};
+    dataSources.forEach(function (source) {
+      Object.assign(merged, typeof source === 'function' ? source.call(this) : source);
+    }, this);
+    return merged;
+  };
   return base;
 };
 window.LearningRegister = function (part) {
